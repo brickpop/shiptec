2fffbceb985b6ef464466fdc6a47d656c33294fe
Jør∂¡ <itsbrickpop@gmail.com>
2021-02-22 21:55:31 +0100

    Fix

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ function checkAvailable() {
 }
 
 function checkAccount(credentials: StockabeeAuth) {
-    if (!credentials) throw new Error("Uninitialized")
+    if (!credentials) throw new Error("Empty credentials")
 
     const url = STOCKABEE_URL_PREFIX + "/account"
     return axios.post(url, credentials)
